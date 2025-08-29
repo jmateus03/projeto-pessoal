eeedfe4a05275485b2a44b470867464411a6e484
João Mateus <joao.sousa1601@aluno.ce.gov.br>
2025-08-28 23:24:15 -0300

    Update script.js

--- script.js
+++ script.js
@@ -25,16 +25,242 @@ alert(`
 ----------------------------------------------------                 
  `)
 
- let dinheiro = prompt("Qual moeda você quer transformar? \n 1.BRL-REAL \n 2.USD-DOLAR AMERICANO \n 3.EUR-EURO \n 4.GBP-LIBRA ESTERLINA \n 5.JPY-IENE JAPÔNES \n 6.AUD-DÓLAR AUSTRALIANO \n 7.CHF- FRANCO SUIÇO \n 8.CAD-DÓLAR CANADENSE \n 9.ARS-PESO ARGENTINO").toLowerCase()
- let dinheiroTranformado = prompt("Em qual moeda seu dinheiro vai ser tranformado? \n 1.BRL-REAL \n 2.USD-DOLAR AMERICANO \n 3.EUR-EURO \n 4.GBP-LIBRA-ESTERLINA \n 5.JPY-IENE JAPÔNES \n 6.AUD-DÓLAR AUSTRALIANO \n 7.CHF- FRANCO SUIÇO \n 8.CAD-DÓLAR CANADENSE \n 9.ARS-PESO ARGENTINO").toLowerCase()
- let dinheiroQuantidade = parseFloat(prompt("Quanto você quer tranformar?"))
+ let dinheiro = prompt("Qual moeda você quer transformar? \n (Coloque apenas o numero) \n 1.REAL \n 2.DOLAR AMERICANO \n 3.EURO \n 4.LIBRA ESTERLINA \n 5.IENE JAPÔNES \n 6.DÓLAR AUSTRALIANO \n 7.FRANCO SUIÇO \n 8.DÓLAR CANADENSE \n 9.PESO ARGENTINO").toLowerCase()
 
- let taxa = undefined
+ let taxa = parseFloat(undefined)
  
- if (dinheiro === "2" || dinheiro === "usd" || dinheiro === "dolar"){
+ if (dinheiro === "1"){
+    taxa = 1.00
+
+    let dinheiroTranformado = prompt("Em qual moeda seu dinheiro vai ser tranformado? \n (Coloque apenas o numero) \n 1.DOLAR AMERICANO \n 2.EURO \n 3.LIBRA-ESTERLINA \n 4.IENE JAPÔNES \n 5.DÓLAR AUSTRALIANO \n 6.FRANCO SUIÇO \n 7.DÓLAR CANADENSE \n 8.PESO ARGENTINO").toLowerCase()
+    let dinheiroQuantidade = parseFloat(prompt("Quanto você quer tranformar?"))
+    
+     if(dinheiroTranformado === "1"){
+        alert(`Você tem ${((dinheiroQuantidade * taxa) /  5).toFixed(2)} dólares`)
+     } else if (dinheiroTranformado === "2"){
+        alert(`Você tem ${((dinheiroQuantidade * taxa) /  5.40).toFixed(2)} euros`)
+     } else if (dinheiroTranformado === "3"){
+        alert(`Você tem ${((dinheiroQuantidade * taxa) /  6.30).toFixed(2)} libras esterlinas`)
+     } else if (dinheiroTranformado === "4"){
+        alert(`Você tem ${((dinheiroQuantidade * taxa) /  0.035).toFixed(2)} ienes japônes`)
+     } else if(dinheiroTranformado === "5"){
+        alert(`Você tem ${((dinheiroQuantidade * taxa) /  3.30).toFixed(2)} dólares australianos`)
+     } else if(dinheiroTranformado === "6"){
+        alert(`Você tem ${((dinheiroQuantidade * taxa) /  5.60).toFixed(2)} francos suiços`)
+     }else if(dinheiroTranformado === "7"){
+        alert(`Você tem ${((dinheiroQuantidade * taxa) / 3.70).toFixed(2)} dólares canadenses`)
+     }else if(dinheiroTranformado === "8"){
+        alert(`Você tem ${((dinheiroQuantidade * taxa) /  0.025).toFixed(2)} pesos argentinos`)
+     } else{
+        alert("Você inseriu uma moeda invalida")
+     }
+
+ }else if (dinheiro === "2"){
     taxa = 5.00
- } else if (dinheiro === "3" || dinheiro === "eur" || dinheiro === "euro"){
+
+    let dinheiroTranformado = prompt("Em qual moeda seu dinheiro vai ser tranformado? \n (Coloque apenas o numero) \n 1.REAL \n 2.EURO \n 3.LIBRA-ESTERLINA \n 4.IENE JAPÔNES \n 5.DÓLAR AUSTRALIANO \n 6.FRANCO SUIÇO \n 7.DÓLAR CANADENSE \n 8.PESO ARGENTINO")
+    let dinheiroQuantidade = parseFloat(prompt("Quanto você quer tranformar?"))
+
+    if(dinheiroTranformado === "1"){
+        alert(`Você tem ${((dinheiroQuantidade * taxa) /  1).toFixed(2)} reais`)
+     } else if (dinheiroTranformado === "2"){
+        alert(`Você tem ${((dinheiroQuantidade * taxa) /  5.40).toFixed(2)} euros`)
+     } else if (dinheiroTranformado === "3"){
+        alert(`Você tem ${((dinheiroQuantidade * taxa) /  6.30).toFixed(2)} libras esterlinas`)
+     } else if (dinheiroTranformado === "4"){
+        alert(`Você tem ${((dinheiroQuantidade * taxa) /  0.035).toFixed(2)} ienes japônes`)
+     } else if(dinheiroTranformado === "5"){
+        alert(`Você tem ${((dinheiroQuantidade * taxa) /  3.30).toFixed(2)} dólares australianos`)
+     } else if(dinheiroTranformado === "6"){
+        alert(`Você tem ${((dinheiroQuantidade * taxa) /  5.60).toFixed(2)} francos suiços`)
+     }else if(dinheiroTranformado === "7"){
+        alert(`Você tem ${((dinheiroQuantidade * taxa) / 3.70).toFixed(2)} dólares canadenses`)
+     }else if(dinheiroTranformado === "8"){
+        alert(`Você tem ${((dinheiroQuantidade * taxa) /  0.025).toFixed(2)} pesos argentinos`)
+     } else{
+        alert("Você inseriu uma moeda invalida")
+     }
+
+ } else if (dinheiro === "3"){
     taxa = 5.40
- }else if(dinheiro === "4" || dinheiro === "gbp" || dinheiro === "libra esterlina"){
+
+     let dinheiroTranformado = prompt("Em qual moeda seu dinheiro vai ser tranformado? \n (Coloque apenas o numero) \n 1.REAL \n 2.DOLARES AMERICANOS  \n 3.LIBRA-ESTERLINA \n 4.IENE JAPÔNES \n 5.DÓLARES AUSTRALIANOS \n 6.FRANCO SUIÇO \n 7.DÓLAR CANADENSE \n 8.PESO ARGENTINO")
+    let dinheiroQuantidade = parseFloat(prompt("Quanto você quer tranformar?"))
+
+    if(dinheiroTranformado === "1"){
+        alert(`Você tem ${((dinheiroQuantidade * taxa) /  1).toFixed(2)} reais`)
+    } else if (dinheiroTranformado === "2"){
+        alert(`Você tem ${((dinheiroQuantidade * taxa) /  5).toFixed(2)} dólares americanos`)
+    } else if (dinheiroTranformado === "3"){
+        alert(`Você tem ${((dinheiroQuantidade * taxa) /  6.30).toFixed(2)} libras esterlinas`)
+    } else if(dinheiroTranformado === "4"){
+       alert(`Você tem ${((dinheiroQuantidade * taxa) /  0.035).toFixed(2)} ienes japôneses`)
+    } else if(dinheiroTranformado === "5"){
+        alert(`Você tem ${((dinheiroQuantidade * taxa) /  3.30).toFixed(2)} dólares australianos`)
+     }else if(dinheiroTranformado === "6"){
+        alert(`Você tem ${((dinheiroQuantidade * taxa) /  5.60).toFixed(2)} francos suiços`)
+     }else if(dinheiroTranformado === "7"){
+        alert(`Você tem ${((dinheiroQuantidade * taxa) / 3.70).toFixed(2)} dólares canadenses`)
+     }else if(dinheiroTranformado === "8"){
+        alert(`Você tem ${((dinheiroQuantidade * taxa) /  0.025).toFixed(2)} pesos argentinos`)
+     } else{
+        alert("Você inseriu uma moeda invalida")
+     }
+
+ } else if(dinheiro === "4"){
     taxa = 6.30
+    
+     let dinheiroTranformado = prompt("Em qual moeda seu dinheiro vai ser tranformado? \n (Coloque apenas o numero) \n 1.REAL \n 2.DOLARES AMERICANOS  \n 3.EURO \n 4.IENE JAPÔNES \n 5.DÓLARES AUSTRALIANOS \n 6.FRANCO SUIÇO \n 7.DÓLAR CANADENSE \n 8.PESO ARGENTINO")
+    let dinheiroQuantidade = parseFloat(prompt("Quanto você quer tranformar?"))
+
+if(dinheiroTranformado === "1"){
+        alert(`Você tem ${((dinheiroQuantidade * taxa) /  1).toFixed(2)} reais`)
+    } else if (dinheiroTranformado === "2"){
+        alert(`Você tem ${((dinheiroQuantidade * taxa) /  5).toFixed(2)} dólares americanos`)
+    } else if (dinheiroTranformado === "3"){
+        alert(`Você tem ${((dinheiroQuantidade * taxa) /  5.40).toFixed(2)} euros`)
+    } else if(dinheiroTranformado === "4"){
+       alert(`Você tem ${((dinheiroQuantidade * taxa) /  0.035).toFixed(2)} ienes japôneses`)
+    } else if(dinheiroTranformado === "5"){
+        alert(`Você tem ${((dinheiroQuantidade * taxa) /  3.30).toFixed(2)} dólares australianos`)
+     }else if(dinheiroTranformado === "6"){
+        alert(`Você tem ${((dinheiroQuantidade * taxa) /  5.60).toFixed(2)} francos suiços`)
+     }else if(dinheiroTranformado === "7"){
+        alert(`Você tem ${((dinheiroQuantidade * taxa) / 3.70).toFixed(2)} dólares canadenses`)
+     }else if(dinheiroTranformado === "8"){
+        alert(`Você tem ${((dinheiroQuantidade * taxa) /  0.025).toFixed(2)} pesos argentinos`)
+     } else{
+        alert("Você inseriu uma moeda invalida")
+     }
+
+ } else if(dinheiro === "5"){
+   taxa = 0.035
+
+        let dinheiroTranformado = prompt("Em qual moeda seu dinheiro vai ser tranformado? \n (Coloque apenas o numero) \n 1.REAL \n 2.DOLARES AMERICANOS  \n 3.EURO \n 4.LIBRAS ESTERLINAS \n 5.DÓLARES AUSTRALIANOS \n 6.FRANCO SUIÇO \n 7.DÓLAR CANADENSE \n 8.PESO ARGENTINO")
+    let dinheiroQuantidade = parseFloat(prompt("Quanto você quer tranformar?"))
+
+if(dinheiroTranformado === "1"){
+        alert(`Você tem ${((dinheiroQuantidade * taxa) /  1).toFixed(2)} reais`)
+    } else if (dinheiroTranformado === "2"){
+        alert(`Você tem ${((dinheiroQuantidade * taxa) /  5).toFixed(2)} dólares americanos`)
+    } else if (dinheiroTranformado === "3"){
+        alert(`Você tem ${((dinheiroQuantidade * taxa) /  5.40).toFixed(2)} euros`)
+    } else if(dinheiroTranformado === "4"){
+       alert(`Você tem ${((dinheiroQuantidade * taxa) /  6.30).toFixed(2)} libras esterlinas`)
+    } else if(dinheiroTranformado === "5"){
+        alert(`Você tem ${((dinheiroQuantidade * taxa) /  3.30).toFixed(2)} dólares australianos`)
+     }else if(dinheiroTranformado === "6"){
+        alert(`Você tem ${((dinheiroQuantidade * taxa) /  5.60).toFixed(2)} francos suiços`)
+     }else if(dinheiroTranformado === "7"){
+        alert(`Você tem ${((dinheiroQuantidade * taxa) / 3.70).toFixed(2)} dólares canadenses`)
+     }else if(dinheiroTranformado === "8"){
+        alert(`Você tem ${((dinheiroQuantidade * taxa) /  0.025).toFixed(2)} pesos argentinos`)
+     } else{
+        alert("Você inseriu uma moeda invalida")
+     }
+
+ } else if(dinheiro === "6"){
+   taxa = 3.30
+
+      let dinheiroTranformado = prompt("Em qual moeda seu dinheiro vai ser tranformado? \n (Coloque apenas o numero) \n 1.REAL \n 2.DOLARES AMERICANOS  \n 3.EURO \n 4.LIBRAS ESTERLINAS \n 5.IENE JAPÔNES \n 6.FRANCO SUIÇO \n 7.DÓLAR CANADENSE \n 8.PESO ARGENTINO")
+    let dinheiroQuantidade = parseFloat(prompt("Quanto você quer tranformar?"))
+
+if(dinheiroTranformado === "1"){
+        alert(`Você tem ${((dinheiroQuantidade * taxa) /  1).toFixed(2)} reais`)
+    } else if (dinheiroTranformado === "2"){
+        alert(`Você tem ${((dinheiroQuantidade * taxa) /  5).toFixed(2)} dólares americanos`)
+    } else if (dinheiroTranformado === "3"){
+        alert(`Você tem ${((dinheiroQuantidade * taxa) /  5.40).toFixed(2)} euros`)
+    } else if(dinheiroTranformado === "4"){
+       alert(`Você tem ${((dinheiroQuantidade * taxa) /  6.30).toFixed(2)} libras esterlinas`)
+    } else if(dinheiroTranformado === "5"){
+        alert(`Você tem ${((dinheiroQuantidade * taxa) /  0.035).toFixed(2)}) ienes japôneses`)
+     }else if(dinheiroTranformado === "6"){
+        alert(`Você tem ${((dinheiroQuantidade * taxa) /  5.60).toFixed(2)} francos suiços`)
+     }else if(dinheiroTranformado === "7"){
+        alert(`Você tem ${((dinheiroQuantidade * taxa) / 3.70).toFixed(2)} dólares canadenses`)
+     }else if(dinheiroTranformado === "8"){
+        alert(`Você tem ${((dinheiroQuantidade * taxa) /  0.025).toFixed(2)} pesos argentinos`)
+     } else{
+        alert("Você inseriu uma moeda invalida")
+     }
+
+ } else if(dinheiro === "7"){
+     taxa = 5.60
+
+        let dinheiroTranformado = prompt("Em qual moeda seu dinheiro vai ser tranformado? \n (Coloque apenas o numero) \n 1.REAL \n 2.DOLARES AMERICANOS  \n 3.EURO \n 4.LIBRAS ESTERLINAS \n 5.IENE JAPÔNES \n 6.DOLAR AUSTRALIANO \n 7.DÓLAR CANADENSE \n 8.PESO ARGENTINO")
+    let dinheiroQuantidade = parseFloat(prompt("Quanto você quer tranformar?"))
+
+if(dinheiroTranformado === "1"){
+        alert(`Você tem ${((dinheiroQuantidade * taxa) /  1).toFixed(2)} reais`)
+    } else if (dinheiroTranformado === "2"){
+        alert(`Você tem ${((dinheiroQuantidade * taxa) /  5).toFixed(2)} dólares americanos`)
+    } else if (dinheiroTranformado === "3"){
+        alert(`Você tem ${((dinheiroQuantidade * taxa) /  5.40).toFixed(2)} euros`)
+    } else if(dinheiroTranformado === "4"){
+       alert(`Você tem ${((dinheiroQuantidade * taxa) /  6.30).toFixed(2)} libras esterlinas`)
+    } else if(dinheiroTranformado === "5"){
+        alert(`Você tem ${((dinheiroQuantidade * taxa) /  0.035).toFixed(2)}) ienes japôneses`)
+     }else if(dinheiroTranformado === "6"){
+        alert(`Você tem ${((dinheiroQuantidade * taxa) /  3.30).toFixed(2)} dólares australianos`)
+     }else if(dinheiroTranformado === "7"){
+        alert(`Você tem ${((dinheiroQuantidade * taxa) / 3.70).toFixed(2)} dólares canadenses`)
+     }else if(dinheiroTranformado === "8"){
+        alert(`Você tem ${((dinheiroQuantidade * taxa) /  0.025).toFixed(2)} pesos argentinos`)
+     } else{
+        alert("Você inseriu uma moeda invalida")
+     }
+
+ } else if(dinheiro === "8"){
+   taxa = 3.70
+
+           let dinheiroTranformado = prompt("Em qual moeda seu dinheiro vai ser tranformado? \n (Coloque apenas o numero) \n 1.REAL \n 2.DOLARES AMERICANOS  \n 3.EURO \n 4.LIBRAS ESTERLINAS \n 5.IENE JAPÔNES \n 6.DOLAR AUSTRALIANO \n 7.FRANCO SUIÇOS \n 8.PESO ARGENTINO")
+    let dinheiroQuantidade = parseFloat(prompt("Quanto você quer tranformar?"))
+
+if(dinheiroTranformado === "1"){
+        alert(`Você tem ${((dinheiroQuantidade * taxa) /  1).toFixed(2)} reais`)
+    } else if (dinheiroTranformado === "2"){
+        alert(`Você tem ${((dinheiroQuantidade * taxa) /  5).toFixed(2)} dólares americanos`)
+    } else if (dinheiroTranformado === "3"){
+        alert(`Você tem ${((dinheiroQuantidade * taxa) /  5.40).toFixed(2)} euros`)
+    } else if(dinheiroTranformado === "4"){
+       alert(`Você tem ${((dinheiroQuantidade * taxa) /  6.30).toFixed(2)} libras esterlinas`)
+    } else if(dinheiroTranformado === "5"){
+        alert(`Você tem ${((dinheiroQuantidade * taxa) /  0.035).toFixed(2)}) ienes japôneses`)
+     }else if(dinheiroTranformado === "6"){
+        alert(`Você tem ${((dinheiroQuantidade * taxa) /  3.30).toFixed(2)} dólares australianos`)
+     }else if(dinheiroTranformado === "7"){
+        alert(`Você tem ${((dinheiroQuantidade * taxa) / 5.60).toFixed(2)} francos suiços`)
+     }else if(dinheiroTranformado === "8"){
+        alert(`Você tem ${((dinheiroQuantidade * taxa) /  0.025).toFixed(2)} pesos argentinos`)
+     } else{
+        alert("Você inseriu uma moeda invalida")
+     }
+
+ } else if(dinheiro === "9"){
+   taxa = 0.025
+
+            let dinheiroTranformado = prompt("Em qual moeda seu dinheiro vai ser tranformado? \n (Coloque apenas o numero) \n 1.REAL \n 2.DOLARES AMERICANOS  \n 3.EURO \n 4.LIBRAS ESTERLINAS \n 5.IENE JAPÔNES \n 6.DOLAR AUSTRALIANO \n 7.FRANCO SUIÇOS \n 8.PESO ARGENTINO")
+    let dinheiroQuantidade = parseFloat(prompt("Quanto você quer tranformar?"))
+
+if(dinheiroTranformado === "1"){
+        alert(`Você tem ${((dinheiroQuantidade * taxa) /  1).toFixed(2)} reais`)
+    } else if (dinheiroTranformado === "2"){
+        alert(`Você tem ${((dinheiroQuantidade * taxa) /  5).toFixed(2)} dólares americanos`)
+    } else if (dinheiroTranformado === "3"){
+        alert(`Você tem ${((dinheiroQuantidade * taxa) /  5.40).toFixed(2)} euros`)
+    } else if(dinheiroTranformado === "4"){
+       alert(`Você tem ${((dinheiroQuantidade * taxa) /  6.30).toFixed(2)} libras esterlinas`)
+    } else if(dinheiroTranformado === "5"){
+        alert(`Você tem ${((dinheiroQuantidade * taxa) /  0.035).toFixed(2)}) ienes japôneses`)
+     }else if(dinheiroTranformado === "6"){
+        alert(`Você tem ${((dinheiroQuantidade * taxa) /  3.30).toFixed(2)} dólares australianos`)
+     }else if(dinheiroTranformado === "7"){
+        alert(`Você tem ${((dinheiroQuantidade * taxa) / 5.60).toFixed(2)} francos suiços`)
+     }else if(dinheiroTranformado === "8"){
+        alert(`Você tem ${((dinheiroQuantidade * taxa) /  3.70).toFixed(2)} dólares canadenses`)
+     } else{
+        alert("Você inseriu uma moeda invalida")
+     }
+
  }
